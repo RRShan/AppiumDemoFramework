d76ae04b873718dab3bfab0b3d937be0aed31668
RRShan <105885016+RRShan@users.noreply.github.com>
2025-07-27 19:38:42 +0530

    Delete PageobjectFDAFW directory

--- PageobjectFDAFW/src/test/java/testRunner/TestRunner.java
+++
@@ -1,9 +0,0 @@
-package testRunner;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/test/java",glue="stepDefinition",
-plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
-public class TestRunner extends AbstractTestNGCucumberTests {
-}
